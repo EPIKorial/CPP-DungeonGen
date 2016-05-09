ceb1c483e3eee2acf1cbaeb1a568c3288364143f
florian huet <florian.huet@epitech.eu>
2016-05-09 12:01:01 +0200

    new map

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 // Login   <korial@epitech.net>
 // 
 // Started on  Wed May  4 12:34:55 2016 Korial
-// Last update Sun May  8 02:01:54 2016 Korial
+// Last update Mon May  9 11:59:15 2016 Korial
 //
 
 #include "map_generator.hpp"
@@ -15,7 +15,7 @@ int	main(int ac, char **av)
 {
   Map	_map;
 
-  _map = Map(25, Size(100, 100), Size(10, 10), Size(5, 5));
+  _map = Map(45, Size(68, 36), Size(15, 8), Size(8, 3));
   _map.generate_map();
   return 0;
 }
